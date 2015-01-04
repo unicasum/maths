9431747a2decee91b165896f51b6ec6b0ff13384
unicasum <info@unicasum.net>
2015-01-04 10:13:44 +0100

    moved some functions to separate js-files

--- emberjs/js/randomBetween.js
+++ emberjs/js/randomBetween.js
@@ -1,7 +1,13 @@
 function randomBetween(a,b) {
-a = a || 0;
-b = b || 10000;
-r=Math.floor(Math.random()*Math.pow(10,b.toString().length));
-//print("a/b/r" + a + " " + b + " " + r );
-return ((r%((b+1)-a))+a);
+  a = a || 0;
+  b = b || 10000;
+
+  if ( a > b ) {
+    c = b;
+    b = a;
+    a = c;
+  }
+
+  r=Math.floor(Math.random()*Math.pow(10,b.toString().length));
+  return ((r%((b+1)-a))+a);
 }
---
+++ emberjs/js/setFocus.js
@@ -0,0 +1,85 @@
+function check(a) {
+  if (( a.toString().length < sum.toString().length ) && ( false == doCheck )) {
+    switch ( a ) {
+      case 1: {
+        document.total.inp_10.focus();
+        break;
+      }
+
+      case 10: {
+        document.total.inp_100.focus();
+        break;
+      }
+
+      case 100: {
+        document.total.inp_1000.focus();
+        break;
+      }
+
+      case 1000: {
+        document.total.inp_10000.focus();
+        break;
+      }
+
+      case 10000: {
+        document.total.inp_100000.focus();
+        break;
+      }
+
+      case 100000: {
+        document.total.inp_1000000.focus();
+        break;
+      }
+
+      case 1000000: {
+        document.total.inp_10000000.focus();
+        break;
+      }
+
+      case 10000000: {
+        document.total.inp_100000000.focus();
+        break;
+      }
+
+      case 100000000: {
+        document.total.inp_1000000000.focus();
+        break;
+      }
+
+      case 1000000000: {
+        document.total.inp_100000000000.focus();
+        break;
+      }
+
+      default: {
+        document.total.inp_1.focus();
+        break;
+      }
+    }
+  } else {
+     var elem = document.getElementById('totalID').elements;
+     var str = "";
+     var errcnt = 0;
+
+     doCheck = true;
+
+     for(var i = 0; i < elem.length; i++) {
+       str += elem[i].value;
+
+       var idx = Math.pow(10,(elem.length-1)-i);
+       if ( chars[i] == elem[i].value ) {
+         document.getElementById('outp_' + idx).innerHTML = '<i class="fa fa-smile-o" style="color:green"></i>';
+       } else {
+         document.getElementById('outp_' + idx).innerHTML = '<i class="fa fa-frown-o" style="color:red"></i>';
+         errcnt ++;
+       }
+     } 
+
+     if ( 0 == errcnt ) {
+       document.getElementById('doReloadID').style.visibility="visible";
+     }
+
+     console.log('input: |' + str + '|');
+     console.log('sum:   |' + sum + '|');
+   }
+ }
